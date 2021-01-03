176c8af59d5826bc2316b995e5d03fae7687d881
CW-B-W <cw.brad.wang@gmail.com>
2021-01-04 01:51:29 +0800

    Implement a branch-free isPowerOfFour
    By replacing `&&` with `&`

--- test2.cpp
+++ test2.cpp
@@ -5,10 +5,18 @@ bool isPowerOfFour(int num)
     return num > 0 && (num & (num - 1)) == 0 && !(__builtin_ctz(num) & 0x1);
 }
 
+bool isPowerOfFour_LessBranch(int num)
+{
+    return (num > 0) & ((num & (num - 1)) == 0) & !(__builtin_ctz(num) & 0x1);
+}
+
 int main()
 {
-    for (int i = 0; i < 1000000; ++i)
+    for (int i = 0; i < 1000000; ++i) {
         if (isPowerOfFour(i))
             printf("%d\n", i);
+        if (isPowerOfFour_LessBranch(i))
+            printf("%d\n", i);
+    }
     return 0;
 }
