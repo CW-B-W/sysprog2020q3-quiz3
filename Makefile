987f567ffcc7049d4f9175a966cf565d60464412
CW-B-W <cw.brad.wang@gmail.com>
2021-01-04 23:32:11 +0800

    Implement multiple versions of gcd64 and benchmark
    version1. gcd64_modop: using % to compute gcd
    version2. gcd64: original version given by jserv
    version3. gcd64_ctz: using __builtin_ctzl()
    
    then use lehmer64() to fast generate 64bit 2^16 random numbers,
    run 100 times and plot the result

--- Makefile
+++ Makefile
@@ -2,13 +2,19 @@ All: test1 test2 test4
 
 test1: ./test1/test1
 
+test1-test: ./test1/test1
+	cd test1 && ./test1
+
 ./test1/test1: ./test1/test1.cpp
 	clang-format -i -style=file $^
 	g++ -fsanitize=undefined -Wall $^ -o $@
 
 test2: ./test2/test2
 
-test2-plot: ./test2/test2
+test2-test: ./test2/test2
+	cd test2 && ./test2
+
+test2-plot: $(wildcard ./test2/*.dat)
 	cd test2 && gnuplot test2-plot.gp
 
 ./test2/test2: ./test2/test2.cpp
@@ -16,6 +22,12 @@ test2-plot: ./test2/test2
 	g++ -Wall $^ -o $@
 
 test4: ./test4/test4
+
+test4-test: ./test4/test4
+	cd test4 && ./test4
+
+test4-plot: $(wildcard ./test4/*.dat)
+	cd test4 && gnuplot test4-plot.gp
 
 ./test4/test4: ./test4/test4.cpp
 	clang-format -i -style=file $^
--- test4/test4.cpp
+++ test4/test4.cpp
@@ -1,7 +1,15 @@
+#include <algorithm>
 #include <cstdint>
 #include <iostream>
 
 using namespace std;
+
+uint64_t gcd64_modop(uint64_t u, uint64_t v)
+{
+    while ((u %= v) && (v %= u))
+        ;
+    return u + v;
+}
 
 uint64_t gcd64(uint64_t u, uint64_t v)
 {
@@ -28,8 +36,81 @@ uint64_t gcd64(uint64_t u, uint64_t v)
     return u << shift;
 }
 
+uint64_t gcd64_ctz(uint64_t u, uint64_t v)
+{
+    if (!u || !v)
+        return u | v;
+
+    int shift = __builtin_ctzl(u | v);
+    u >>= shift;
+    v >>= shift;
+
+    u >>= __builtin_ctzl(u);
+
+    do {
+        v >>= __builtin_ctzl(v);
+
+        if (u < v) {
+            v -= u;
+        }
+        else {
+            uint64_t t = u - v;
+            u = v;
+            v = t;
+        }
+    } while (v);
+    return u << shift;
+}
+
+static inline uint64_t splitmix64_stateless(uint64_t index)
+{
+    uint64_t z = (index + UINT64_C(0x9E3779B97F4A7C15));
+    z = (z ^ (z >> 30)) * UINT64_C(0xBF58476D1CE4E5B9);
+    z = (z ^ (z >> 27)) * UINT64_C(0x94D049BB133111EB);
+    return z ^ (z >> 31);
+}
+uint64_t lehmer64()
+{
+    const uint64_t seed = 65521; /* prime */
+    static __uint128_t g_lehmer64_state =
+        (((__uint128_t)splitmix64_stateless(seed)) << 64) +
+        splitmix64_stateless(seed + 1);
+    g_lehmer64_state *= 0xda942042e4dd58b5;
+    return g_lehmer64_state >> 64;
+}
+
+int bench()
+{
+    uint64_t *test_data = new uint64_t[1 << (16 + 1)];
+    for (int i = 0; i < (1 << 16); ++i) {
+        test_data[i * 2 + 0] = lehmer64();
+        test_data[i * 2 + 1] = lehmer64();
+    }
+
+    uint64_t (*test_func[])(uint64_t, uint64_t) = {gcd64_modop, gcd64,
+                                                   gcd64_ctz};
+    FILE *f_list[sizeof(test_func) / sizeof(bool (*)(int))];
+    for (long unsigned f = 0; f < sizeof(test_func) / sizeof(bool (*)(int));
+         ++f) {
+        char filepath[32];
+        sprintf(filepath, "./func%lu.dat", f);
+        f_list[f] = fopen(filepath, "w");
+        for (int t = 0; t < 100; ++t) {
+            clock_t start = clock();
+            for (int tt = 0; tt < (1 << 16); ++tt) {
+                test_func[f](test_data[tt * 2 + 0], test_data[tt * 2 + 1]);
+            }
+            clock_t end = clock();
+            fprintf(f_list[f], "%d %f\n", t,
+                    ((double)end - start) / CLOCKS_PER_SEC);
+        }
+    }
+    delete[] test_data;
+    return 0;
+}
+
 int main()
 {
-    cout << gcd64((1UL << 62), (1UL << 37)) << endl;
+    bench();
     return 0;
 }
